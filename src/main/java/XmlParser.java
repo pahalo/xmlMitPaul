34069dbf74e70ff55f907c2e78edfccd5388ae0e
Unf4ir <paulclasht@gmail.com>
2023-12-20 15:45:03 +0100

    Writing in the XML

--- src/main/java/XmlParser.java
+++ src/main/java/XmlParser.java
@@ -1,20 +1,12 @@
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import java.io.*;
+import org.w3c.dom.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerFactory;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
 import org.xml.sax.SAXException;
-import java.io.File;
-import java.io.IOException;
+
 
 public class XmlParser {
 
@@ -23,47 +15,43 @@ public class XmlParser {
             // XML-Dokument erstellen und parsen
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse("employee.xml");
+            Document document = builder.parse("src/main/resources/employee.xml");
             document.getDocumentElement().normalize();
 
-            // Iteration durch die Employee-Elemente
+            // Erstellen einer temporären Datei zum Zwischenspeichern
+            File tempFile = new File("temp_employee.xml");
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+
+            // Speichern des originalen Dokuments in der temporären Datei
+            DOMSource source = new DOMSource(document);
+            StreamResult tempResult = new StreamResult(tempFile);
+            transformer.transform(source, tempResult);
+
+            // Durchführung von Änderungen im Dokument
             NodeList employeeList = document.getElementsByTagName("employee");
             for (int i = 0; i < employeeList.getLength(); i++) {
                 Node employee = employeeList.item(i);
                 if (employee.getNodeType() == Node.ELEMENT_NODE) {
                     Element employeeElement = (Element) employee;
-                    System.out.println("Employee ID: " + employeeElement.getAttribute("id"));
 
                     // Hinzufügen der Position "Software Developer" zu jedem Employee
                     Element positionElement = document.createElement("position");
                     positionElement.appendChild(document.createTextNode("Software Developer"));
                     employeeElement.appendChild(positionElement);
-
-                    // Ausgabe der Details jedes Employees
-                    NodeList employeeDetails = employee.getChildNodes();
-                    for (int j = 0; j < employeeDetails.getLength(); j++) {
-                        Node detail = employeeDetails.item(j);
-                        if (detail.getNodeType() == Node.ELEMENT_NODE) {
-                            Element detailElement = (Element) detail;
-                            System.out.println(
-                                    "        " + detailElement.getTagName() + ": " + detailElement.getTextContent());
-                        }
-                    }
                 }
             }
 
-            // Transformer, um das aktualisierte Dokument zu speichern
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(new File("employee.xml"));
-            try {
-                transformer.transform(source, result);
-                System.out.println("Anreicherung der XML Datei erfolgreich");
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        } catch (ParserConfigurationException | IOException | SAXException e) {
+            // Aktualisiertes Dokument in die Originaldatei schreiben
+            DOMSource updatedSource = new DOMSource(document);
+            StreamResult result = new StreamResult(new File("src/main/resources/employee.xml"));
+            transformer.transform(updatedSource, result);
+            System.out.println("Anreicherung der XML Datei erfolgreich");
+
+            // Löschen der temporären Datei
+            tempFile.delete();
+
+        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
             e.printStackTrace();
         }
     }
