909eb6c096a8d027133f640da41a9ace94ee8e6c
Unf4ir <paulclasht@gmail.com>
2023-12-21 09:48:10 +0100

    Integrating JDOM

--- src/main/java/XmlParser.java
+++ src/main/java/XmlParser.java
@@ -1,58 +1,151 @@
-import java.io.*;
-import org.w3c.dom.*;
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import javax.xml.parsers.*;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import org.xml.sax.SAXException;
-
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 
 public class XmlParser {
 
-    public static void main(String[] args) throws TransformerConfigurationException {
+    public static void main(String[] args) {
+    	Scanner scanner = new Scanner (System.in);
+    	// This main method is just an example for demonstration purposes
+        // You can use it to test the getSAXParsedDocument method
+        String fileName = "src/main/resources/employees.xml";
+        Document document = getSAXParsedDocument("src/main/resources/employees.xml");
+        Element rootNode = document.getRootElement();
+        
+    	List<String> actions = Arrays.asList("Liste ausgeben", "Liste ergänzen", "Listeninhalt löschen?");
+    	System.out.println("Was willst du machen?");
+    	for (String action : actions) {
+            System.out.println(action);
+        }
+    	String choosenAction = scanner.nextLine();
+    	if(choosenAction.equals("1")) {
+    		rootNode.getChildren("employee").forEach(XmlParser::readEmployeeNode );
+    	} else if(choosenAction.equals("2")) {
+    		addEmployee(rootNode, document);
+    	} else if(choosenAction.equals("3")) {
+    		deleteEmployee(rootNode,document);
+    	}
+    	System.out.println("");
+        callMainMethod();
+		scanner.close();
+    }
+
+    public static Document getSAXParsedDocument(final String fileName) {
+        SAXBuilder builder = new SAXBuilder();
+        Document document = null;
         try {
-            // XML-Dokument erstellen und parsen
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document document = builder.parse("src/main/resources/employee.xml");
-            document.getDocumentElement().normalize();
+            document = builder.build(fileName);
+            
+        } catch (JDOMException | IOException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+    private static void readEmployeeNode(Element employeeNode) {
 
-            // Erstellen einer temporären Datei zum Zwischenspeichern
-            File tempFile = new File("temp_employee.xml");
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
+    	//Employee Id
+    	System.out.println("Id : " + employeeNode.getAttributeValue("id"));
 
-            // Speichern des originalen Dokuments in der temporären Datei
-            DOMSource source = new DOMSource(document);
-            StreamResult tempResult = new StreamResult(tempFile);
-            transformer.transform(source, tempResult);
+    	//First Name
+    	System.out.println("FirstName : " + employeeNode.getChildText("firstName"));
 
-            // Durchführung von Änderungen im Dokument
-            NodeList employeeList = document.getElementsByTagName("employee");
-            for (int i = 0; i < employeeList.getLength(); i++) {
-                Node employee = employeeList.item(i);
-                if (employee.getNodeType() == Node.ELEMENT_NODE) {
-                    Element employeeElement = (Element) employee;
+    	//Last Name
+    	System.out.println("LastName : " + employeeNode.getChildText("lastName"));
 
-                    // Hinzufügen der Position "Software Developer" zu jedem Employee
-                    Element positionElement = document.createElement("position");
-                    positionElement.appendChild(document.createTextNode("Software Developer"));
-                    employeeElement.appendChild(positionElement);
-                }
-            }
+    	//Country
+    	System.out.println("country : " + employeeNode.getChild("country").getText());
 
-            // Aktualisiertes Dokument in die Originaldatei schreiben
-            DOMSource updatedSource = new DOMSource(document);
-            StreamResult result = new StreamResult(new File("src/main/resources/employee.xml"));
-            transformer.transform(updatedSource, result);
-            System.out.println("Anreicherung der XML Datei erfolgreich");
+    	/**Read Department Content*/
+    	employeeNode.getChildren("department").forEach( XmlParser::readDepartmentNode );
+    }
+    private static void readDepartmentNode(Element deptNode) {
 
-            // Löschen der temporären Datei
-            tempFile.delete();
+    	//Department Id
+    	System.out.println("Department Id : " + deptNode.getAttributeValue("id"));
 
-        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
+    	//Department Name
+    	System.out.println("Department Name : " + deptNode.getChildText("name"));
+    }
+    private static void addEmployee(Element rootNode, Document document) {
+    	Scanner scanner = new Scanner(System.in);
+    	Element newEmployee = new Element("employee"); 	
+    	List<Element> children = rootNode.getChildren();
+    	newEmployee.setAttribute("id", String.valueOf(children.size() +1));
+    	
+    	System.out.println("First name?");
+    	String inputFirstName = scanner.nextLine();
+    	Element firstName = new Element("firstName").setText(inputFirstName);
+    	newEmployee.addContent(firstName);
+    	
+    	System.out.println("Last name?");
+    	String inputLastName = scanner.nextLine();
+    	Element lastName = new Element("lastName").setText(inputLastName);
+    	newEmployee.addContent(lastName);
+    	
+    	System.out.println("Country?");
+    	String inputcountry = scanner.nextLine();
+    	Element country = new Element("country").setText(inputcountry);
+        newEmployee.addContent(country);
+
+        rootNode.addContent(newEmployee);
+        document.setContent(rootNode);
+
+        save(document);
+    }
+    private static void deleteEmployee(Element rootNode,Document document) {
+    	System.out.print("Welche ID soll gefeuert werden?");
+    	Scanner scanner = new Scanner(System.in);
+    	String inputID = scanner.nextLine();
+    	List<Element> employees = rootNode.getChildren("employee");
+    	boolean employeeFound = false;
+    	Element employeeToRemove = null;
+    	
+    	
+    	for(Element employee : employees) {
+    		if (employee.getAttributeValue("id").equals(inputID)) {
+    			System.out.println("Employee ID: " + employee.getAttributeValue("id"));
+                System.out.println("First Name: " + employee.getChildText("firstName"));
+                System.out.println("Last Name: " + employee.getChildText("lastName"));
+                System.out.println("Country: " + employee.getChildText("country"));
+                employeeFound = true;
+                employeeToRemove = employee;
+                break;
+    		}
+    	}
+    	if (!employeeFound) {
+            System.out.println("Employee with ID " + inputID + " not found.");
+        } else {
+        	System.out.println("Delete this Employee?");
+        	String confirmation = scanner.nextLine();
+        	if(confirmation.equals("yes")) {
+        		rootNode.removeContent(employeeToRemove);
+        		save(document);
+        	}
+        }
+    	rootNode.getChildren("employee").forEach(XmlParser::readEmployeeNode );
+    }
+    
+    private static void save(Document document) {
+    	try {
+            XMLOutputter xmlOutput = new XMLOutputter();
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(document, new FileWriter("src/main/resources/employees.xml"));
+            System.out.println("Mitarbeiter erfolgreich hinzugefügt und XML-Datei aktualisiert!");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public static void callMainMethod() {
+            String[] arguments = {};
+            main(arguments);
+        }
 }
