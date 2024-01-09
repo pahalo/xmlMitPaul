f29290dcbed9f941b08395ae93b2e941c7f63491
Unf4ir <paulclasht@gmail.com>
2024-01-09 12:02:22 +0100

    New Task number 1

--- src/main/java/XMLProcessor.java
+++ src/main/java/XMLProcessor.java
@@ -7,23 +7,21 @@ import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.Namespace;
 
+/**
+ * This class searches tif for duplicates in the MPI import
+ */
 public class XMLProcessor {
-    // Liste von Duplikaten der 'xlink:href'-Attribute
-    private static List<String> hrefDuplicatesList = new ArrayList<>();
-    // Zähler für Dateien mit Duplikaten und Gesamtanzahl der Duplikate
-    private static int filesWithDuplicates = 0;
-    private static int totalDuplicates = 0;
 
     public static void main(String[] args) {
-        // Überprüfung, ob ein Verzeichnis angegeben wurde
+    	int filesWithDuplicates = 0;
+        int totalDuplicates = 0;
+        
         if (args.length != 1) {
             System.out.println("Bitte nur ein Verzeichnis angeben.");
         } else {
             File directory = new File(args[0]).getAbsoluteFile();
-            // Überprüfung, ob das Verzeichnis existiert und ein Verzeichnis ist
             if (directory.exists() && directory.isDirectory()) {
-                processFiles(directory);
-                // Ausgabe der Anzahl der Dateien mit Duplikaten und Gesamtanzahl der Duplikate
+                processFiles(directory, filesWithDuplicates, totalDuplicates);
                 System.out.println("Anzahl der Dateien mit Duplikaten: " + filesWithDuplicates);
                 System.out.println("Gesamtanzahl der Duplikate: " + totalDuplicates);
             } else {
@@ -32,24 +30,36 @@ public class XMLProcessor {
         }
     }
 
-    // Verarbeitung von Dateien im Verzeichnis und seinen Unterverzeichnissen
-    private static void processFiles(File directory) {
+    /**
+     * Recursively traverses all files and directories in the specified directory and processes XML files.
+     * @param directory The directory to be searched.
+     * @param filesWithDuplicates Number of files with duplicates.
+     * @param totalDuplicates Total count of duplicates.
+     */
+    
+    private static void processFiles(File directory, int filesWithDuplicates, int totalDuplicates) {
         if (directory.isDirectory()) {
             File[] files = directory.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isFile() && file.getName().toLowerCase().endsWith(".xml")) {
-                        processXmlFile(file, directory);
+                        processXmlFile(file, directory, filesWithDuplicates, totalDuplicates);
                     } else if (file.isDirectory()) {
-                        processFiles(file);
+                        processFiles(file, filesWithDuplicates, totalDuplicates);
                     }
                 }
             }
         }
     }
+    /**
+     * Processes a single XML file and collects all XML elements.
+     * @param file The XML file to be processed.
+     * @param directory The directory where the XML file resides.
+     * @param filesWithDuplicates Number of files with duplicates.
+     * @param totalDuplicates Total count of duplicates.
+     */
 
-    // Verarbeitung einer XML-Datei
-    private static void processXmlFile(File file, File directory) {
+    private static void processXmlFile(File file, File directory, int filesWithDuplicates, int totalDuplicates) {
         List<String> xmlElementsList = new ArrayList<>();
 
         try {
@@ -57,13 +67,17 @@ public class XMLProcessor {
             Document doc = sax.build(file);
             Element rootElement = doc.getRootElement();
             collectXmlElements(rootElement, file, xmlElementsList);
-            findDuplicates(xmlElementsList, file);
+            findDuplicates(xmlElementsList, file,  filesWithDuplicates, totalDuplicates);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
-    // Sammeln von XML-Elementen aus einer XML-Datei
+    /**
+     * Collects XML elements recursively.
+     * @param element The XML element being processed.
+     * @param xmlFile The XML file currently being processed.
+     * @param xmlElementsList List to collect XML elements.
+     */
     private static void collectXmlElements(Element element, File xmlFile, List<String> xmlElementsList) {
         if (element != null) {
             StringBuilder elementString = new StringBuilder();
@@ -71,14 +85,12 @@ public class XMLProcessor {
             Namespace ns = element.getNamespace(); 
             String prefix = ns.getPrefix(); 
 
-            // Erstellen der XML-Elementzeichenfolge mit Präfix und Attributen
             if (!prefix.isEmpty()) {
                 elementString.append("<").append(prefix).append(":").append(element.getName());
             } else {
                 elementString.append("<").append(element.getName());
             }
 
-            // Hinzufügen der Attribute zum XML-Element
             List<Attribute> attributes = element.getAttributes();
             for (Attribute attribute : attributes) {
                 elementString.append(" ").append(attribute.getQualifiedName()).append("=\"").append(attribute.getValue()).append("\"");
@@ -86,7 +98,6 @@ public class XMLProcessor {
 
             List<Element> children = element.getChildren();
 
-            // Rekursive Verarbeitung von Kind-Elementen
             if (!children.isEmpty()) {
                 elementString.append(">");
 
@@ -110,10 +121,19 @@ public class XMLProcessor {
             }
         }
     }
-
-    private static void findDuplicates(List<String> xmlElementsList, File xmlFile) {
+    
+    /**
+     * Finds duplicates of xlink:href attributes within XML elements.
+     * @param xmlElementsList List of XML elements.
+     * @param xmlFile The XML file currently being processed.
+     * @param filesWithDuplicates Number of files with duplicates.
+     * @param totalDuplicates Total count of duplicates.
+     */
+    
+    private static void findDuplicates(List<String> xmlElementsList, File xmlFile, int filesWithDuplicates, int totalDuplicates) {
         boolean duplicatesFound = false;
         Set<String> hrefs = new HashSet<>();
+        List<String> hrefDuplicatesList = new ArrayList<>();
 
         for (String element : xmlElementsList) {
             int hrefIndex = element.indexOf("xlink:href=\"");
@@ -124,7 +144,6 @@ public class XMLProcessor {
                     String href = element.substring(start, end);
                     if (!hrefs.add(href)) {
                         duplicatesFound = true;
-                        // Überprüfen und Hinzufügen von Duplikaten der 'xlink:href'-Attribute
                         if (!hrefDuplicatesList.contains(element)) {
                             hrefDuplicatesList.add(href);
                         }
@@ -133,7 +152,6 @@ public class XMLProcessor {
             }
         }
 
-        // Hier werden die extrahierten ID-Werte für die Duplikate ausgegeben
         if (duplicatesFound) {
         	filesWithDuplicates++;
             totalDuplicates += hrefDuplicatesList.size();
